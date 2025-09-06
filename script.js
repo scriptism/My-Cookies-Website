2f0a357342f1eee53717e38989465bd09eb06b96
Amin <aminullahpuya@gmail.com>
2025-09-06 07:24:10 +0200

    updated some functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container ul");
 const h2 = document.querySelector(".container h2");
 const h1 = document.querySelector("h1");
 const p = document.querySelector("p");
+const refresh = document.querySelector(".refresh");
 
 button.addEventListener("click", () => {
   container.style.backgroundColor = "red";
@@ -13,4 +14,10 @@ button.addEventListener("click", () => {
   h2.textContent = "More Coming Soon!";
   h2.style.color = "blue";
   p.style.display = "none";
+  refresh.style.display = "block";
+  button.style.display = "none";
+});
+
+refresh.addEventListener("click", () => {
+  location.reload();
 });
